ea05ab59b575497a80d6fb29a87c3dde69ab2066
Rray <3111937934@qq.com>
2026-09-19 15:27:07 +0800

    Create house-robber.cpp

---
+++ hot100/house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size=static_cast<int>(nums.size());
+        if(size==1) return nums[0];
+        if(size==2) return max(nums[0],nums[1]);
+        vector<int>dp(size+1,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<size;++i){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[size-1];
+    }
+};
